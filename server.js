0ddeaddbf835d3cc75510e7aa310c5071844127e
Guillaume Rousseau <guillaume.rousseau@epitech.eu>
2018-01-12 16:36:58 +0100

    save results on file ok

--- server.js
+++ server.js
@@ -7,6 +7,43 @@ var tags_right = [];
 var tags_left = [];
 var act_question = "oui ou non ?";
 
+function write_score_to_file(left, right)
+{
+  fs.writeFile(__dirname + "/save.json", {"left": JSON.stringify(left), "right": JSON.stringify(right)}, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+  }); 
+}
+
+function read_score_from_file()
+{
+  fs.readFile(__dirname + '/save.json', function (err, data) {
+    if (err) {
+      return console.log('Error loading');
+    }
+
+    if (data && data != "") {
+      try {
+        var ret = JSON.parse(data);
+        if (ret && ret.left && ret.right){
+          tags_right = ret.right;
+          tags_left = ret.left;
+        }
+        else {
+          console.log("json object invalid");
+        }
+      } catch (e) {
+        console.log("file not a json file");
+      }
+    }
+  });
+}
+
+read_score_from_file();
+
 /*
 ** server connect to dashboard
 */
@@ -31,6 +68,7 @@ function handler (req, res) {
     if (tags_right.indexOf(tag) < 0 && tags_left.indexOf(tag) < 0) {
       console.log("add tag left");
       tags_left.push(tag);
+      write_score_to_file(tags_left, tags_right);
       io.emit("left", tags_left.length);
       dash.emit("get_form", [tags_left.length, tags_right.length]);
     }
@@ -47,6 +85,7 @@ function handler (req, res) {
     if (tags_right.indexOf(tag) < 0 && tags_left.indexOf(tag) < 0) {
       console.log("add tag right");
       tags_right.push(tag);
+      write_score_to_file(tags_left, tags_right);
       io.emit("right", tags_right.length);
       dash.emit("get_form", [tags_left.length, tags_right.length]);
     }
@@ -84,8 +123,17 @@ dash.on("set_question", function(question) {
 
 dash.on("get_question", function() {
 	console.log("sending question to dashboard");
-	dash.emit("get_question", act_question);
+  dash.emit("get_question", act_question);
 })
+
+dash.on("reset_score", function(){
+  tags_left = [];
+  tags_right = [];
+  //write on file
+  write_score_to_file(tags_left, tags_right);
+  io.emit("left", tags_left.length);
+  io.emit("right", tags_right.length);  
+});
 
 io.on('connection', function (socket) {
   io.emit("left", tags_left.length);
